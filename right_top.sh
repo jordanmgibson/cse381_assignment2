63c077409cac31b13e6879fdee6b0c3b25e18c1d
gibson26 <gibson26@miamioh.edu>
2022-04-08 19:15:25 -0400

    Add new file

---
+++ right_top.sh
@@ -0,0 +1,18 @@
+i = $2
+while [ i -ge 1 ]
+do
+    j = i
+    while [ j -gt i]
+    do
+        echo " "
+        j -= 1
+    done
+    k = 1
+    while [ k -ge i ]
+    do
+        echo "$1"
+        k += 1
+    done
+    echo
+    i -= 1
+done
