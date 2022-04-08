e7027666c1f9c7bb7b6d1e98529260640504e06d
gibson26 <gibson26@miamioh.edu>
2022-04-08 19:28:38 -0400

    Update right_top.sh

--- right_top.sh
+++ right_top.sh
@@ -1,17 +1,17 @@
 i=$2
 while [ $i -ge 1 ]
 do
-    $j=$i
-    while [ $j -gt i]
+    j = $i
+    while [ $j -gt $i]
     do
         echo " "
-        $j-=1
+        $j = $j - 1
     done
     k=1
     while [ $k >= $i ]
     do
         echo "$1"
-        $k+=1
+        $k = $k + 1
     done
     echo
     $i-=1
