7ef0dae32763d37c7eb58300d606d1cda0a55106
gibson26 <gibson26@miamioh.edu>
2022-04-08 19:33:52 -0400

    Update right_top.sh

--- right_top.sh
+++ right_top.sh
@@ -1,14 +1,14 @@
 i=$2
-while [ $i -ge 1 ]
+while [ $i >= 1 ]
 do
     $j=$2
-    while [ $j -gt $i]
+    while [ $j > $i]
     do
         echo " "
         j=`expr $j - 1`
     done
     k=1
-    while [ $k -ge $i ]
+    while [ $k >= $i ]
     do
         echo "$1"
         k=`expr $k + 1`
