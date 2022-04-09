9b5b9898fe0da1ef377ec3c6288e013a32f5f052
gibson26 <gibson26@miamioh.edu>
2022-04-08 20:47:13 -0400

    Update hollow_square.sh

--- hollow_square.sh
+++ hollow_square.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+for (( i = 1; i <= `expr $2 + 1`; i++ ))
+do
+    for (( j = 1; j <= `expr $2 + 1`; j++ ))
+    do
+        if [ $i == 1 ] || [ $i == $2 ] || [ $j == 1] || [ $j == $2 ]
+        then
+            echo "$1"
+        else
+            echo " "
+        fi
+    done
+    echo
+done
