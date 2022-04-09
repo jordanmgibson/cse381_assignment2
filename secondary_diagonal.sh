85007f4d055953dd2c9c345ac57e25d0616134f9
gibson26 <gibson26@miamioh.edu>
2022-04-08 20:57:43 -0400

    Update secondary_diagonal.sh

--- secondary_diagonal.sh
+++ secondary_diagonal.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+for (( i = 1; i < `expr $2 + 1`; i++ ))
+do
+    for (( j = 1; j < `expr $2 + 1`; j++ ))
+    do
+        if [ $i+$j == `expr $2 + 1` ]
+        then
+            echo -n "$1"
+        else
+            echo -n " "
+        fi
+    done
+    echo
+done
