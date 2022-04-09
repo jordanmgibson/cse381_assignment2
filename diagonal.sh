7b4286368e4055d27394ea80b5520c3c5380a564
gibson26 <gibson26@miamioh.edu>
2022-04-08 20:08:46 -0400

    Update diagonal.sh

--- diagonal.sh
+++ diagonal.sh
@@ -1,1 +1,13 @@
-
+for (( i = 1; i <= $2 + 1; i++ ))
+do
+    for (( j = 1; i <= $2 + 1; i++ ))
+    do
+        if [ i -eq j ]
+        then
+            echo -n "$1"
+        else
+            echo -n " "
+        fi
+    done
+done
+echo
