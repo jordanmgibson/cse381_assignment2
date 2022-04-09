c8f62441a12f64fc37fb71fcf78964f2ab8edadc
gibson26 <gibson26@miamioh.edu>
2022-04-08 21:07:14 -0400

    Update hollow_square.sh, secondary_diagonal.sh

--- hollow_square.sh
+++ hollow_square.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for (( i = 1; i < `expr $2 + 1`; i++ ))
+for (( i = 1; i <= $2; i++ ))
 do
-    for (( j = 1; j < `expr $2 + 1`; j++ ))
+    for (( j = 1; j <= $2; j++ ))
     do
         if [ $i -eq 1 ] || [  $i -eq $2 ] || [ $j -eq 1 ] || [ $j -eq $2 ]
         then
